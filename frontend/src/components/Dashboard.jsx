502c648d3081e285e34356ecf6752e8aebacfdcd
abdul khalik <abdul.khalik@alliedglobetech.com>
2026-02-20 11:12:40 +0530

    created a dashboard with live count using api

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,48 @@
+function Dashboard({ alerts }) {
+    const total = alerts.length;
+    console.log(alerts);
+  
+    const highSeverity = alerts.filter(
+        (a) => a.details?.severity === 3
+      ).length;
+      
+      const mediumSeverity = alerts.filter(
+        (a) => a.details?.severity === 2
+      ).length;
+      
+      const lowSeverity = alerts.filter(
+        (a) => a.details?.severity === 1
+      ).length;
+  
+    return (
+      <div className="dashboard-stats">
+        {/* <h3>Dashboard</h3> */}
+  
+        <div className="dashboard-cards">
+  <div className="stat-card total">
+    <h4>Total Alerts</h4>
+    <p>{total}</p>
+  </div>
+
+  <div className="stat-card high">
+    <h4>High Severity</h4>
+    <p>{highSeverity}</p>
+  </div>
+
+  <div className="stat-card medium">
+    <h4>Medium Severity</h4>
+    <p>{mediumSeverity}</p>
+  </div>
+
+  <div className="stat-card low">
+    <h4>Low Severity</h4>
+    <p>{lowSeverity}</p>
+  </div>
+</div>
+
+      </div>
+    );
+  }
+  
+  export default Dashboard;
+  
